27768f0dcf717d355555745fc943b3bca485d59c
Eric Connell <ericconnell94@gmail.com>
2022-09-08 17:18:06 -0700

    converted div tag to h1 tag on app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const App = () => {
   return (
-    <div>
+    <h1>
       Journeyman - a trader or crafter who has completed an apprenticeship but
       is not at the level of a master craftsman
-    </div>
+    </h1>
   );
 };
