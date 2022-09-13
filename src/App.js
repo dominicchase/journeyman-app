483b43bdaae6a74a28120fbd7ca0ebf19ba7d6d8
dominicchase <46654604+dominicchase@users.noreply.github.com>
2022-09-12 23:44:28 -0400

    Setup commerce-js and created dummy products in Catalog component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+
+import { Catalog } from "./components/Catalog";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/global.css";
 
 export const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="fw-bold">
-      Journeyman - a trader or crafter who has completed an apprenticeship but
-      is not at the level of a master craftsman
+    <div>
+      <div className="fw-bold">
+        Journeyman - a trader or crafter who has completed an apprenticeship but
+        is not at the level of a master craftsman
+      </div>
+      <Catalog products={products} setProducts={setProducts} />
     </div>
   );
 };
